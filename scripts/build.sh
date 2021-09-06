f0c3316152e14e9b05a10ca4b6cf73656142af4d
Evgeniy <284661@niuitmo.ru>
2021-09-06 14:58:39 +0300

    artifacts and libs added

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-artifacts=../../java-advanced-2021/artifacts
-lib=../../java-advanced-2021/lib
+artifacts=../java-advanced-2021/artifacts
+lib=../java-advanced-2021/lib
 path=$artifacts/info.kgeorgiy.java.advanced.base.jar:$artifacts/info.kgeorgiy.java.advanced.implementor.jar:$lib/junit-4.11.jar
 cd ../java-solutions || exit
 javac --module-path=$path info/kgeorgiy/ja/kosogorov/implementor/Implementor.java module-info.java -d java.solutions
